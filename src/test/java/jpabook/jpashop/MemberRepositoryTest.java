fbb834e9bd332fac746ba30529ad9f065dc3e8c2
pozafly <pozafly@gmail.com>
2025-01-18 14:14:50 +0900

    feat: yaml 작성 및 repository test

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        /**
+         * hashCode, toString을 정의하지 않았으므로 == 비교임.
+         * 하지만, findMember, member는 주소 값이 다를텐데?
+         * 하지만, 같은 영속성 context 안에서는 이게 같음.
+         * 쿼리
+         */
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
