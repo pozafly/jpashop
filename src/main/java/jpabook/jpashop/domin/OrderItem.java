4d0fa82623520f018bde36181bc911d5f5bd94c2
pozafly <pozafly@gmail.com>
2025-03-16 20:27:52 +0900

    feat: 엔티티 개발

---
+++ src/main/java/jpabook/jpashop/domin/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domin;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domin.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int orderPrice;
+
+    private int count;
+}
---
+++ src/main/java/jpabook/jpashop/domin/OrderStatus.java
@@ -0,0 +1,5 @@
+package jpabook.jpashop.domin;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
