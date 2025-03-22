cc535b95a75735d6683ac7a1dcef11f4bf601e2f
pozafly <pozafly@gmail.com>
2025-03-22 17:48:38 +0900

    feat: Member Service, Repository 구현

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,17 +1,13 @@
 package jpabook.jpashop;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JpashopApplicationTests {
-
-    @Autowired
-    MemberRepository memberRepository;
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+// @SpringBootTest
+// class JpashopApplicationTests {
+//
+//     @Autowired
+//     MemberRepository memberRepository;
+//
+//     @Test
+//     void contextLoads() {
+//     }
+//
+// }
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domin.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("회원가입")
+    public void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+        em.flush();
+        // then
+        Assert.assertEquals(member, memberRepository.findOne(savedId));
+
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @DisplayName("중복_회원_예외")
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+        memberService.join(member2); // 예외가 발생해야 함!
+
+        // then
+        Assert.fail("예외가 발생해야 한다.");
+    }
+}
