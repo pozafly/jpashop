e0fefeb51218bbaa5b98a24361c9a53bba066455
pozafly <pozafly@gmail.com>
2025-05-05 14:38:10 +0900

    feat: spring data jpa 맛보기

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -38,12 +38,12 @@ public class MemberService {
     }
 
     public Member findById(Long id) {
-        return memberRepository.findOne(id);
+        return memberRepository.findById(id).get();
     }
 
     @Transactional
     public void update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
